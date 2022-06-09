3eea0c2be9e43059764fe9e15deb4c532f71f4fb
Owen Hayes <owen.v.hayes@gmail.com>
2022-06-09 15:09:19 -0700

    Initial commit.

---
+++ src/test/java/com/marvelousanything/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvelousanything.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
